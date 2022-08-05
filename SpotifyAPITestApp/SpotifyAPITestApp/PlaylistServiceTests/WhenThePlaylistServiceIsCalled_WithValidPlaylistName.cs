94439a65549826d18565b0b66ecade55bc21866f
ThommCroft <thomas.wolstencroft@outlook.com>
2022-08-05 16:36:41 +0100

    Created Playlist helper methods and tests. Playlist ID test fails due to actual result not mathing expected result.

--- SpotifyAPITestApp/SpotifyAPITestApp/PlaylistServiceTests/WhenThePlaylistServiceIsCalled_WithValidPlaylistName.cs
+++ SpotifyAPITestApp/SpotifyAPITestApp/PlaylistServiceTests/WhenThePlaylistServiceIsCalled_WithValidPlaylistName.cs
@@ -17,9 +17,47 @@ namespace SpotifyAPITestApp.PlaylistServiceTests
         }
 
         [Test]
-        public void i()
+        public void IDOfPlaylist_Is6QuKSXmuJpiHZec1mYrKwZ()
         {
-            Assert.That(_playlistService.PlaylistResponseDTO.Response.name, Is.EqualTo("New Playlist"));
+            Assert.That(_playlistService.GetPlaylistID(), Is.EqualTo("6QuKSXmuJpiHZec1mYrKwZ"));
+        }
+
+        [Test]
+        public void NameOfPlaylist_IsNewPlaylist()
+        {
+            // PlaylistResponseDTO.Response.name
+            Assert.That(_playlistService.GetPlaylistName(), Is.EqualTo("New Playlist"));
+        }
+
+        [Test]
+        public void TotalPlaylistFollowers_IsZero()
+        {
+            Assert.That(_playlistService.GetPlaylistTotalFollowers(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void PlaylistCollabartive_IsFalse()
+        {
+            Assert.That(_playlistService.CheckIfPlaylistIsCollaborative(), Is.EqualTo(false));
+        }
+
+        [Test]
+        public void OwnerDisplayNameOfPlaylist_IsGuns_n_Spartans()
+        {
+            Assert.That(_playlistService.GetPlaylistOwnerDisplayName(), Is.EqualTo("Guns-n-Spartans"));
+        }
+
+        [Test]
+        public void PlaylistTotalTracks_IsZero()
+        {
+            Assert.That(_playlistService.GetPlaylistTotalTracks(), Is.EqualTo(0));
+        }
+
+        [Test]
+        [Ignore("Not Implemented")]
+        public void NameOfTrackInPlaylist_IsTrackNameHere()
+        {
+            //Assert.That(_playlistService.GetPlaylistName(), Is.EqualTo("New Playlist"));
         }
     }
 }
